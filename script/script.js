7c9741d5fa68be7410d79896dede37ab2a49d977
ricardo.m21 <ricardoabraham00@gmail.com>
2023-05-04 00:21:00 -0300

    agregando secciones

--- script/script.js
+++
@@ -1,7 +0,0 @@
-const aside= document.getElementById('aside'),
-menu=document.getElementById('menu')
-
-menu.onclick=()=>{
-    console.log("LE DISTE CLICK")
-    aside.classList.toggle('active');
-}
